5bc1710a1a7f98df6dbaf53efc8695a4382254b4
HarshhT29 <132380397+HarshhT29@users.noreply.github.com>
2025-01-18 01:42:57 +0530

    Time: 15 ms (84.65%), Space: 45.7 MB (80.33%) - LeetHub

--- 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.java
+++ 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.java
@@ -1,44 +1,34 @@
 class Solution {
     public boolean canBeValid(String s, String locked) {
         int n = s.length();
-        if((n&1)!=0) {
+        if (n % 2 != 0) {
             return false;
         }
-        int open=0, unlocked=0;
-        for(int i=0;i<n;i++) {
-            if(locked.charAt(i)=='0') {
-                unlocked++;
-            } else if(s.charAt(i)=='(') {
+
+        int open = 0;
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == '(' || locked.charAt(i) == '0') {
                 open++;
-            } else if(s.charAt(i)==')') {
-                if(open>0) {
-                    open--;
-                } else if(unlocked>0) {
-                    unlocked--;
-                } else {
-                    return false;
-                }
+            } else {
+                open--;
+            }
+            if (open < 0) {
+                return false;
             }
         }
 
-        int bal=0;
-        for(int i=n-1;i>=0;i--) {
-            if(locked.charAt(i)=='0') {
-                bal--;
-                unlocked--;
-            } else if(s.charAt(i)=='(') {
-                open--;
+        int bal = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (s.charAt(i) == ')' || locked.charAt(i) == '0') {
                 bal++;
-            } else if(s.charAt(i)==')') {
+            } else {
                 bal--;
             }
-            if(bal>0) {
+            if (bal < 0) {
                 return false;
             }
-            if(open==0 && unlocked==0) {
-                return true;
-            }
         }
-        return !(open>0);
+
+        return true;
     }
 }
