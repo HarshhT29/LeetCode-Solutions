9b816ba0e93f05534028b030c2f1d04cf0b58d81
HarshhT29 <132380397+HarshhT29@users.noreply.github.com>
2025-01-23 22:23:17 +0530

    Time: 2 ms (100%), Space: 52.3 MB (69.49%) - LeetHub

--- 1396-count-servers-that-communicate/1396-count-servers-that-communicate.java
+++ 1396-count-servers-that-communicate/1396-count-servers-that-communicate.java
@@ -1,47 +1,30 @@
 class Solution {
-
     public int countServers(int[][] grid) {
-        int numRows = grid.length;
-        int numCols = numRows > 0 ? grid[0].length : 0;
-        int communicableServersCount = 0;
+        int n = grid.length;
+        int m = grid[0].length;
 
-        // Traverse through the grid
-        for (int row = 0; row < numRows; ++row) {
-            for (int col = 0; col < numCols; ++col) {
-                if (grid[row][col] == 1) {
-                    boolean canCommunicate = false;
+        int[] row = new int[m];
+        int[] col = new int[n];
+        int cnt = 0;
 
-                    // Check for communication in the same row
-                    for (int otherCol = 0; otherCol < numCols; ++otherCol) {
-                        if (otherCol != col && grid[row][otherCol] == 1) {
-                            canCommunicate = true;
-                            break;
-                        }
-                    }
-
-                    // If a server was found in the same row, increment
-                    // communicableServersCount
-                    if (canCommunicate) {
-                        communicableServersCount++;
-                    } else {
-                        // Check for communication in the same column
-                        for (int otherRow = 0; otherRow < numRows; ++otherRow) {
-                            if (otherRow != row && grid[otherRow][col] == 1) {
-                                canCommunicate = true;
-                                break;
-                            }
-                        }
-
-                        // If a server was found in the same column, increment
-                        // communicableServersCount
-                        if (canCommunicate) {
-                            communicableServersCount++;
-                        }
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                if(grid[i][j]==1) {
+                    row[j]++;
+                    col[i]++;
+                }
+            }
+        }
+        
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                if(grid[i][j]==1) {
+                    if(row[j]>1 || col[i]>1) {
+                        cnt++;
                     }
                 }
             }
         }
-
-        return communicableServersCount;
+        return cnt;
     }
 }
