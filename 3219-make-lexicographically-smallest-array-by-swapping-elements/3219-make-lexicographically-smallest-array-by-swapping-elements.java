3e3a9818629bb48b6d28d088431de135ce79e1ab
HarshhT29 <132380397+HarshhT29@users.noreply.github.com>
2025-01-26 16:56:25 +0530

    Time: 88 ms (58.33%), Space: 77.7 MB (8.33%) - LeetHub

--- 3219-make-lexicographically-smallest-array-by-swapping-elements/3219-make-lexicographically-smallest-array-by-swapping-elements.java
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/3219-make-lexicographically-smallest-array-by-swapping-elements.java
@@ -1,8 +1,14 @@
 class Solution {
+    //Elements swappable grp banate hai, yahin swapping sirf
+    //same grp ke elements mei hi possible hai.
+    //Saare grp elements ko aise arrange karo ki vo 
+    //actual array mei relatively sorted rahe
+
     public int[] lexicographicallySmallestArray(int[] nums, int limit) {
         List<Deque<Integer>> groups = new ArrayList<>();
-        Map<Integer, Integer> mapNumToGrp = new HashMap<>();
-
+        Map<Integer, Integer> mapNumToGrp = new HashMap<>(); //map all nums[i] with there respective group
+        
+        //Sort karne ke baad sare grp elements sath mei aajayenge
         List<Integer> sortedNums = new ArrayList<>(nums.length);
         for(int num:nums) {
             sortedNums.add(num);
@@ -17,6 +23,7 @@ class Solution {
             mapNumToGrp.put(sNum, groups.size()-1);
         }
 
+        //Dequeue se phela element utha te jao
         int[] res = new int[nums.length];
         for(int i=0;i<nums.length;i++) {
             int grpIndex = mapNumToGrp.get(nums[i]);
