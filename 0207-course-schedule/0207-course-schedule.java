e20703282a9cc2a1b808441299407eff5c1a57b2
HarshhT29 <132380397+HarshhT29@users.noreply.github.com>
2025-01-27 23:10:51 +0530

    Time: 3 ms (99.84%), Space: 45.6 MB (49.98%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,30 +1,38 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        int[] indegree = new int[numCourses];
-        
-        for(int[] pair:prerequisites) {
-            indegree[pair[0]]++;
-        }
-        List<Integer> topoOrder = new ArrayList<>();
-        Queue<Integer> q = new LinkedList<>();
-        for(int i=0;i<indegree.length;i++) {
-            if(indegree[i]==0) {
-                q.add(i);
+        List<Integer>[] adjList = new List[numCourses];
+        for (int[] pair : prerequisites) {
+            if (adjList[pair[1]] == null) {
+                adjList[pair[1]] = new ArrayList<>();
             }
+            adjList[pair[1]].add(pair[0]);
         }
+        int[] visited = new int[numCourses];
 
-        while(!q.isEmpty()) {
-            int curr = q.remove();
-            topoOrder.add(curr);
-
-            for(int[] pair:prerequisites) {
-                if(curr==pair[1]) {
-                    if(--indegree[pair[0]]==0) {
-                        q.add(pair[0]);
-                    }
+        for (int i = 0; i < numCourses; i++) {
+            if (visited[i] == 0) {
+                if (detectCycle(adjList, visited, i)) {
+                    return false;
                 }
             }
         }
-        return topoOrder.size()==numCourses;
+
+        return true;
+    }
+
+    private boolean detectCycle(List<Integer>[] adjList, int[] visited, int src) {
+        if(visited[src]!=0) {
+            return visited[src]==1;
+        }
+        visited[src]=1;
+        if(adjList[src]!=null) {
+            for(int node:adjList[src]) {
+            if(detectCycle(adjList, visited, node)) {
+                return true;
+            }
+        }
+        }
+        visited[src]=2;
+        return false;
     }
 }
