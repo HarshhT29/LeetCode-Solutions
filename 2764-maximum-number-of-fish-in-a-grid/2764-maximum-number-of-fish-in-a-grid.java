3dc2b7a2f19421a351824f85506f0d2cb29c89c6
HarshhT29 <132380397+HarshhT29@users.noreply.github.com>
2025-01-28 19:16:10 +0530

    Time: 3 ms (100%), Space: 45.4 MB (13.73%) - LeetHub

--- 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.java
+++ 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.java
@@ -2,32 +2,33 @@ class Solution {
     public int findMaxFish(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        boolean[][] visited = new boolean[n][m];
+        
         int max = 0;
         for(int i=0;i<n;i++) {
             for(int j=0;j<m;j++) {
-                if(!visited[i][j] && grid[i][j]!=0) {
-                    max = Math.max(max, helper(grid, visited, i, j));
+                if(grid[i][j]>0) {
+                    max = Math.max(max, helper(grid, i, j));
                 }
             }
         }
 
         return max;
     }
-    private int helper(int[][] grid, boolean[][] visited, int i, int j) {
-        if(i>=grid.length || j>=grid[0].length || i<0 || j<0) {
+    private int helper(int[][] grid, int i, int j) {
+        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == 0) {
             return 0;
         }
-        if(grid[i][j]==0 || visited[i][j]) {
+        if(grid[i][j]==0) {
             return 0;
         }
-        visited[i][j] = true;
         
-        int c = helper(grid, visited, i, j+1);
-        c+= helper(grid, visited, i, j-1);
-        c+= helper(grid, visited, i+1, j);
-        c+= helper(grid, visited, i-1, j);
-
-        return c+grid[i][j];
+        int c = grid[i][j];
+        grid[i][j]=0;
+        c += helper(grid, i, j+1);
+        c += helper(grid, i, j-1);
+        c += helper(grid, i+1, j);
+        c += helper(grid, i-1, j);
+        
+        return c;
     }
 }
