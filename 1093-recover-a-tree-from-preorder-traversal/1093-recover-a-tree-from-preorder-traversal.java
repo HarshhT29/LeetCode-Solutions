e6fc27cf1205752d7fe0cb3a708bc3692fec70a2
HarshhT29 <132380397+HarshhT29@users.noreply.github.com>
2025-02-23 19:03:49 +0530

    Time: 7 ms (42.54%), Space: 45.2 MB (13.43%) - LeetHub

--- 1093-recover-a-tree-from-preorder-traversal/1093-recover-a-tree-from-preorder-traversal.java
+++ 1093-recover-a-tree-from-preorder-traversal/1093-recover-a-tree-from-preorder-traversal.java
@@ -1,66 +1,56 @@
-// /**
-//  * Definition for a binary tree node.
-//  * public class TreeNode {
-//  *     int val;
-//  *     TreeNode left;
-//  *     TreeNode right;
-//  *     TreeNode() {}
-//  *     TreeNode(int val) { this.val = val; }
-//  *     TreeNode(int val, TreeNode left, TreeNode right) {
-//  *         this.val = val;
-//  *         this.left = left;
-//  *         this.right = right;
-//  *     }
-//  * }
-//  */
-// class Solution {
-//     public TreeNode recoverFromPreorder(String traversal) {
-        
-//     }
-// }
-
-public class Solution {
-
-    static int index = 0;
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
     public TreeNode recoverFromPreorder(String traversal) {
-        index = 0;
-        return helper(traversal, 0);
-    }
+        Stack<TreeNode> st = new Stack<>();
+        int n = traversal.length();
+        int i = 0;
 
-    private TreeNode helper(String traversal, int depth) {
-        if (index >= traversal.length()) return null;
-        // Count the number of dashes
-        int dashCount = 0;
-        while (
-            (index + dashCount) < traversal.length() &&
-            traversal.charAt(index + dashCount) == '-'
-        ) {
-            dashCount++;
+        while(i<n) {
+            int depth = 0;
+            while(i<n && traversal.charAt(i)=='-') {
+                depth++;
+                i++;
+            }
+
+            int val = 0;
+            while(i<n && traversal.charAt(i)>='0' && traversal.charAt(i)<='9') {
+                val = val*10 + (traversal.charAt(i)-'0');
+                i++;
+            }
+
+            TreeNode node = new TreeNode(val);
+
+            while(st.size()>depth) {
+                st.pop();
+            }
+
+            if(!st.isEmpty()) {
+                TreeNode top = st.peek();
+                if(top.left==null) {
+                    top.left = node;
+                } else {
+                    top.right = node;
+                }
+            }
+            st.push(node);
         }
-
-        // If the number of dashes doesn't match the current depth, return null
-        if (dashCount != depth) return null;
-
-        // Move index past the dashes
-        index += dashCount;
-
-        // Extract the node value
-        int value = 0;
-        while (
-            index < traversal.length() &&
-            Character.isDigit(traversal.charAt(index))
-        ) {
-            value = value * 10 + (traversal.charAt(index++) - '0');
+        while(st.size()>1) {
+            st.pop();
         }
-
-        // Create the current node
-        TreeNode node = new TreeNode(value);
-
-        // Recursively build the left and right subtrees
-        node.left = helper(traversal, depth + 1);
-        node.right = helper(traversal, depth + 1);
-
-        return node;
+        return st.peek();
     }
 }
