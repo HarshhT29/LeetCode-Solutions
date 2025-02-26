901a288e5971fa0d8215e312ac882e3f6e3a22f9
HarshhT29 <132380397+HarshhT29@users.noreply.github.com>
2025-02-26 13:41:56 +0530

    Time: 3 ms (97.88%), Space: 57 MB (14.02%) - LeetHub

--- 1849-maximum-absolute-sum-of-any-subarray/1849-maximum-absolute-sum-of-any-subarray.java
+++ 1849-maximum-absolute-sum-of-any-subarray/1849-maximum-absolute-sum-of-any-subarray.java
@@ -5,18 +5,21 @@ class Solution {
     private int kadaneAlgo(int[] nums, String minMax) {
         int limit = minMax.equals("max")?Integer.MIN_VALUE:Integer.MAX_VALUE;
         int currSum = 0;
-        for(int num:nums) {
-            currSum += num;
-            if(minMax.equals("min")) {
+        if(minMax.equals("min")) {
+            for(int num:nums) {
+                currSum += num;
                 limit = Math.min(limit, currSum);
                 if(currSum>0) {
                     currSum = 0;
                 }
-            } else {
-                limit = Math.max(limit, currSum);
-                if(currSum<0) {
-                    currSum = 0;
-                }
+            }
+            return limit;
+        }
+        for(int num:nums) {
+            currSum += num;
+            limit = Math.max(limit, currSum);
+            if(currSum<0) {
+                currSum = 0;
             }
         }
         return limit;
