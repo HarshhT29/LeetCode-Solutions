d09aaa59fad70ec2aec18b94b6cf07672787e414
HarshhT29 <132380397+HarshhT29@users.noreply.github.com>
2025-03-02 05:42:25 +0530

    Time: 1 ms (86.98%), Space: 45.6 MB (31.44%) - LeetHub

--- 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.java
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.java
@@ -2,6 +2,7 @@ class Solution {
     public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
         List<int[]> li = new ArrayList<>();
         int i=0, j=0;
+
         while(i<nums1.length && j<nums2.length) {
             if(nums1[i][0]==nums2[j][0]) {
                 li.add(new int[] { nums1[i][0], nums1[i++][1]+nums2[j++][1] });
@@ -11,17 +12,20 @@ class Solution {
                 li.add(nums1[i++]);
             }
         }
+
         while(i<nums1.length) {
             li.add(nums1[i++]);
         }
         while(j<nums2.length) {
             li.add(nums2[j++]);
         }
+
         i=0;
         int[][] res = new int[li.size()][2];
         for(int[] nums:li) {
             res[i++] =  nums;
         }
+        
         return res;
     }
 }
