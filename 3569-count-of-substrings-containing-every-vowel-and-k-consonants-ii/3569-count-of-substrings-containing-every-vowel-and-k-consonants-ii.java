fb0adbd3379fbe7f25d5c689d245ee6cbd04efb9
HarshhT29 <132380397+HarshhT29@users.noreply.github.com>
2025-03-11 18:39:47 +0530

    Time: 299 ms (28.21%), Space: 46 MB (77.78%) - LeetHub

--- 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii.java
+++ 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii.java
@@ -1,36 +1,30 @@
 class Solution {
     public long countOfSubstrings(String word, int k) {
-        int[] nextCon = getNextConsonant(word.toCharArray());
+        return helper(word, k) - helper(word, k+1);
+    }
+    private boolean isVowel(char ch) {
+        return ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u';
+    }
+    private long helper(String s, int k) {
         Map<Character, Integer> map = new HashMap<>();
-        int n = word.length();
-        int i=0, j=0;
-        long cnt = 0, con=0;
+        int n = s.length();
+        int i=0,j=0;
+        long cnt = 0, con = 0;
 
         while(j<n) {
-            if(!isVowel(word.charAt(j))) {
+            if(!isVowel(s.charAt(j))) {
                 con++;
             } else {
-                map.put(word.charAt(j), map.getOrDefault(word.charAt(j), 0)+1);
+                map.put(s.charAt(j), map.getOrDefault(s.charAt(j), 0)+1);
             }
-            while(con>k) {
-                if(!isVowel(word.charAt(i))) {
+            while(map.size()==5 && con>=k) {
+                cnt += n-j;
+                if(!isVowel(s.charAt(i))) {
                     con--;
                 } else {
-                    map.put(word.charAt(i), map.get(word.charAt(i))-1);
-                    if(map.get(word.charAt(i))==0) {
-                        map.remove(word.charAt(i));
-                    }
-                }
-                i++;
-            }
-            while(i<n && map.size()==5 && con==k) {
-                cnt = cnt + nextCon[j]-j;
-                if(!isVowel(word.charAt(i))) {
-                    con--;
-                } else {
-                    map.put(word.charAt(i), map.get(word.charAt(i))-1);
-                    if(map.get(word.charAt(i))==0) {
-                        map.remove(word.charAt(i));
+                    map.put(s.charAt(i), map.get(s.charAt(i))-1);
+                    if(map.get(s.charAt(i))==0) {
+                        map.remove(s.charAt(i));
                     }
                 }
                 i++;
@@ -39,19 +33,5 @@ class Solution {
         }
         return cnt;
     }
-    private int[] getNextConsonant(char[] arr) {
-        int[] nums = new int[arr.length];
-        nums[nums.length-1] = nums.length;
-        for(int i=nums.length-2;i>=0;i--) {
-            if(!isVowel(arr[i+1])) {
-                nums[i] = i+1;
-            } else {
-                nums[i] = nums[i+1];
-            }
-        }
-        return nums;
-    }
-    private boolean isVowel(char ch) {
-        return ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u';
-    }
+
 }
