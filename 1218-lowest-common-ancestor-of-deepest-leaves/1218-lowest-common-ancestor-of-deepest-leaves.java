8cd05bba0b2a2613508a92e45cfee006f1679ada
HarshhT29 <132380397+HarshhT29@users.noreply.github.com>
2025-04-10 02:39:13 +0530

    Time: 2 ms (13.49%), Space: 44.8 MB (11.65%) - LeetHub

--- 1218-lowest-common-ancestor-of-deepest-leaves/1218-lowest-common-ancestor-of-deepest-leaves.java
+++ 1218-lowest-common-ancestor-of-deepest-leaves/1218-lowest-common-ancestor-of-deepest-leaves.java
@@ -13,32 +13,37 @@
  *     }
  * }
  */
-// class Solution {
-//     public TreeNode lcaDeepestLeaves(TreeNode root) {
-        
-//     }
-// }
-
 class Solution {
-
     public TreeNode lcaDeepestLeaves(TreeNode root) {
-        return dfs(root).getKey();
+        Map<TreeNode, Integer> map = new HashMap<>();
+        dfs(root, map);
+
+        return findLCA(root, map);
     }
-
-    private Pair<TreeNode, Integer> dfs(TreeNode root) {
-        if (root == null) {
-            return new Pair<>(null, 0);
+    private int dfs(TreeNode root, Map<TreeNode, Integer> map) {
+        if(root==null) {
+            return 0;
         }
+        int left = dfs(root.left, map);
+        int right = dfs(root.right, map);
+        int depth = Math.max(left, right)+1;
 
-        Pair<TreeNode, Integer> left = dfs(root.left);
-        Pair<TreeNode, Integer> right = dfs(root.right);
-
-        if (left.getValue() > right.getValue()) {
-            return new Pair<>(left.getKey(), left.getValue() + 1);
+        map.put(root, depth);
+        return depth;
+    }
+    private TreeNode findLCA(TreeNode root, Map<TreeNode, Integer> map) {
+        if(root==null) {
+            return null;
         }
-        if (left.getValue() < right.getValue()) {
-            return new Pair<>(right.getKey(), right.getValue() + 1);
+        int left = root.left!=null?map.get(root.left):0;
+        int right = root.right!=null?map.get(root.right):0;
+        
+        if(left==right) {
+            return root;
+        } else if(left>right) {
+            return findLCA(root.left, map);
+        } else {
+            return findLCA(root.right, map);
         }
-        return new Pair<>(root, left.getValue() + 1);
     }
 }
