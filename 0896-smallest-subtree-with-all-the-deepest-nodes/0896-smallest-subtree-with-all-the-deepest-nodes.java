2c894255768aeedfd78dc1b2f66b1310781e0995
HarshhT29 <132380397+HarshhT29@users.noreply.github.com>
2025-04-10 02:40:31 +0530

    Time: 1 ms (24.08%), Space: 41.5 MB (84.64%) - LeetHub

---
+++ 0896-smallest-subtree-with-all-the-deepest-nodes/0896-smallest-subtree-with-all-the-deepest-nodes.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode subtreeWithAllDeepest(TreeNode root) {
+        Map<TreeNode, Integer> map = new HashMap<>();
+        dfs(root, map);
+
+        return findLCA(root, map);
+    }
+    private int dfs(TreeNode root, Map<TreeNode, Integer> map) {
+        if(root==null) {
+            return 0;
+        }
+        int left = dfs(root.left, map);
+        int right = dfs(root.right, map);
+        int depth = Math.max(left, right)+1;
+
+        map.put(root, depth);
+        return depth;
+    }
+    private TreeNode findLCA(TreeNode root, Map<TreeNode, Integer> map) {
+        if(root==null) {
+            return null;
+        }
+        int left = root.left!=null?map.get(root.left):0;
+        int right = root.right!=null?map.get(root.right):0;
+        
+        if(left==right) {
+            return root;
+        } else if(left>right) {
+            return findLCA(root.left, map);
+        } else {
+            return findLCA(root.right, map);
+        }
+    }
+}
