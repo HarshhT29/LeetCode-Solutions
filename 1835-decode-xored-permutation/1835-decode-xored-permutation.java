b7cf1c553c602006517cdc54e6a3918584450d55
HarshhT29 <132380397+HarshhT29@users.noreply.github.com>
2025-04-23 22:49:45 +0530

    Time: 3 ms (32.76%), Space: 64.7 MB (18.97%) - LeetHub

--- 1835-decode-xored-permutation/1835-decode-xored-permutation.java
+++ 1835-decode-xored-permutation/1835-decode-xored-permutation.java
@@ -14,7 +14,19 @@ class Solution {
         for(int i=1;i<perm.length;i++) {
             perm[i] = perm[i-1]^encoded[i-1];
         }
-        
+
         return perm;
     }
 }
+/*
+NOTE:
+encoded[i] = perm[i]^pern[i+1];
+n=5 perm = {a, b, c, d, e}
+encoded[0] = a^b
+encoded[1] = b^c
+encoded[2] = c^d
+encoded[3] = d^e
+implies xor of odd index will have xor of first natural number except first element 
+perm[0] found
+then, perm[i] = perm[i-1]^encoded[i-1];
+*/
