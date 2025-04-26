b46cb3ce11dde5b3efa291d9cd02654ae2baa922
HarshhT29 <132380397+HarshhT29@users.noreply.github.com>
2025-04-27 00:58:01 +0530

    Time: 0 ms (100%), Space: 40.1 MB (91.84%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.java
+++ 0052-n-queens-ii/0052-n-queens-ii.java
@@ -1,41 +1,35 @@
 class Solution {
-    private List<List<String>> res;
-    public int totalNQueens(int n) {
-        res = new ArrayList<>();
-        solve(n, 0, new ArrayList<>());
+    private boolean[] rows;
+    private boolean[] d1;
+    private boolean[] d2;
+    private int cnt;
 
-        return res.size();
+    public int totalNQueens(int n) {
+        rows = new boolean[n];
+        d1 = new boolean[2*n];
+        d2 = new boolean[2*n];
+        cnt = 0;
+        backtrack(n, 0);
+        return cnt;
     }
-    private void solve(int n, int col, List<String> li) {
+    private void backtrack(int n, int col) {
         if(col==n) {
-            res.add(new ArrayList<>(li));
-            return ;
+            cnt++;
+            return ; 
         }
         for(int i=0;i<n;i++) {
-            if(isSafe(li, i, col)) {
-                li.add(generateRow(n, i));
-                solve(n, col+1, li);
-                li.remove(li.size()-1);
+            if(rows[i] || d1[i-col+n] || d2[i+col]) {
+                continue;
             }
+            rows[i] = true;
+            d1[i-col+n] = true;
+            d2[i+col] = true;
+
+            backtrack(n, col+1);
+
+            rows[i] = false;
+            d1[i-col+n] = false;
+            d2[i+col] = false;
         }
-    }
-    private boolean isSafe(List<String> board, int row, int col) {
-        for(int i=0;i<board.size();i++) {
-            int rowQ = board.get(i).indexOf('Q');
-            if(rowQ==row) {
-                return false;
-            }
-            if(Math.abs(col-i)==Math.abs(row-rowQ)) {
-                return false;
-            }
-        }
-        return true;
-    }
-    private String generateRow(int n, int pos) {
-        StringBuilder s = new StringBuilder();
-        for(int i=0;i<n;i++) {
-            s.append(i==pos?'Q':'.');
-        }
-        return s.toString();
     }
 }
