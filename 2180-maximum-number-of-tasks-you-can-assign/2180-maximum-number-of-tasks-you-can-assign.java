05ef9bf215f22d486d0e45a3a3a018f498b437aa
HarshhT29 <132380397+HarshhT29@users.noreply.github.com>
2025-05-01 15:36:04 +0530

    Time: 64 ms (76.33%), Space: 54.9 MB (97.55%) - LeetHub

---
+++ 2180-maximum-number-of-tasks-you-can-assign/2180-maximum-number-of-tasks-you-can-assign.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int maxTaskAssign(int[] tasks, int[] workers, int pills, int strength) {
+        Arrays.sort(tasks);
+        Arrays.sort(workers);
+        
+        int low = 0;
+        int high = Math.min(tasks.length, workers.length);
+        int result = 0;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (canAssignKtasks(mid, tasks, workers, pills, strength)) {
+                result = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return result;
+    }
+    
+    private boolean canAssignKtasks(int k, int[] tasks, int[] workers, int pills, int strength) {
+        if (k == 0) return true;
+        
+        Deque<Integer> deque = new ArrayDeque<>();
+        int workerIndex = workers.length - 1;
+        
+        for (int i = k - 1; i >= 0; i--) {
+            while (workerIndex >= workers.length - k && workers[workerIndex] + strength >= tasks[i]) {
+                deque.addFirst(workers[workerIndex]);
+                workerIndex--;
+            }
+            
+            if (deque.isEmpty()) {
+                return false;
+            }
+            
+            if (deque.getLast() >= tasks[i]) {
+                deque.removeLast();
+            } else {
+                if (pills<=0) {
+                    return false;
+                }
+                pills--;
+                deque.removeFirst();
+            }
+        }
+        return true;
+    }
+}
