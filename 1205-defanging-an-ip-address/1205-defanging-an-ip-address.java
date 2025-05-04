23e16c93d4ed46a0a1988315764198a9701ef5b3
HarshhT29 <132380397+HarshhT29@users.noreply.github.com>
2025-05-05 00:00:29 +0530

    Time: 0 ms (100%), Space: 41.5 MB (76.18%) - LeetHub

--- 1205-defanging-an-ip-address/1205-defanging-an-ip-address.java
+++ 1205-defanging-an-ip-address/1205-defanging-an-ip-address.java
@@ -3,9 +3,7 @@ class Solution {
         StringBuilder defanged = new StringBuilder();
         for(char ch:address.toCharArray()) {
             if(ch=='.') {
-                defanged.append("[");
-                defanged.append(".");
-                defanged.append("]");
+                defanged.append("[.]");
             } else {
                 defanged.append(ch);
             }
