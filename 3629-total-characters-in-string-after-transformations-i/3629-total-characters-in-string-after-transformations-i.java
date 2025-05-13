3054af192afdb67ca0f2f8d18861df627f49f21e
HarshhT29 <132380397+HarshhT29@users.noreply.github.com>
2025-05-14 00:08:46 +0530

    Time: 248 ms (35.44%), Space: 45.6 MB (72.15%) - LeetHub

---
+++ 3629-total-characters-in-string-after-transformations-i/3629-total-characters-in-string-after-transformations-i.java
@@ -0,0 +1,29 @@
+class Solution {
+    private final int MOD = 1_000_000_007;
+
+    public int lengthAfterTransformations(String s, int t) {
+        long[] freq = new long[26];
+        for(char ch:s.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+
+        while(t-->0) {
+            long[] nextFreq = new long[26];
+            for(int i=0;i<26;i++) {
+                if(i==25) {
+                    nextFreq[0] = (nextFreq[0] + freq[25]) % MOD;
+                    nextFreq[1] = (nextFreq[1] + freq[25]) % MOD;
+                } else {
+                    nextFreq[i+1] = (nextFreq[i+1] + freq[i]) % MOD;
+                }
+            }
+            freq = nextFreq;
+        }
+
+        long result = 0;
+        for (long count:freq) {
+            result = (result+count) % MOD;
+        }
+        return (int)result;
+    }
+}
