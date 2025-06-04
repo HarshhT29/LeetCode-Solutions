a77cb2b58eec5343a5a2626bd69d553092b07fe8
HarshhT29 <132380397+HarshhT29@users.noreply.github.com>
2025-06-04 21:32:22 +0530

    Time: 0 ms (100%), Space: 46.4 MB (49.82%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,18 +1,37 @@
 class Solution {
+    // public int trap(int[] height) {
+    //     int[] leftMax = new int[height.length];
+    //     int[] rightMax = new int[height.length];
+    //     leftMax[0] = height[0];
+    //     for(int i=1;i<leftMax.length;i++) {
+    //         leftMax[i] = Math.max(leftMax[i-1], height[i]);
+    //     }
+    //     rightMax[rightMax.length-1] = height[height.length-1];
+    //     for(int i=rightMax.length-2;i>=0;i--) {
+    //         rightMax[i] = Math.max(rightMax[i+1], height[i]);
+    //     }
+    //     int water = 0;
+    //     for(int i=1;i<height.length-1;i++) {
+    //         water += (Math.min(rightMax[i], leftMax[i])-height[i]);
+    //     }
+    //     return water;
+    // }
+
     public int trap(int[] height) {
-        int[] leftMax = new int[height.length];
-        int[] rightMax = new int[height.length];
-        leftMax[0] = height[0];
-        for(int i=1;i<leftMax.length;i++) {
-            leftMax[i] = Math.max(leftMax[i-1], height[i]);
-        }
-        rightMax[rightMax.length-1] = height[height.length-1];
-        for(int i=rightMax.length-2;i>=0;i--) {
-            rightMax[i] = Math.max(rightMax[i+1], height[i]);
-        }
+        int i = 0;
+        int j = height.length-1;
+        int leftMax = 0, rightMax = 0;
         int water = 0;
-        for(int i=1;i<height.length-1;i++) {
-            water += (Math.min(rightMax[i], leftMax[i])-height[i]);
+        while(i<j) {
+            if(height[j] > height[i]) {
+                leftMax = Math.max(leftMax, height[i]);
+                water += leftMax - height[i];
+                i++;
+            } else {
+                rightMax = Math.max(rightMax, height[j]);
+                water += rightMax - height[j];
+                j--;
+            }
         }
         return water;
     }
