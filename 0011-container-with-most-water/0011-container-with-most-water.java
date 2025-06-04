45b0a3844903272b3160858f6d182065eb589b49
HarshhT29 <132380397+HarshhT29@users.noreply.github.com>
2025-06-04 17:15:52 +0530

    Time: 5 ms (75.34%), Space: 57.8 MB (64.5%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -3,9 +3,8 @@ class Solution {
         int i = 0, j = height.length-1;
         int max = 0;
         while(i<height.length && j>=0) {
-            int wid = j-i;
             int h = Math.min(height[i], height[j]);
-            max = Math.max(max, wid*h);
+            max = Math.max(max, h*(j-i));
             if(height[i]>height[j]) {
                 j--;
             } else {
