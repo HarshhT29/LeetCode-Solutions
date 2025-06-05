2fa35b2f5264d0668c5f10f1b275b5838b5a9258
HarshhT29 <132380397+HarshhT29@users.noreply.github.com>
2025-06-05 10:42:10 +0530

    Time: 0 ms (100%), Space: 42.2 MB (60.19%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -2,7 +2,9 @@ class Solution {
     public void rotate(int[][] matrix) {
         for(int i=0;i<matrix.length;i++) {
             for(int j=i+1;j<matrix.length;j++) {
-                swap(matrix, i, j);
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
         }
         for(int i=0;i<matrix.length;i++) {
@@ -15,10 +17,5 @@ class Solution {
                 high--;
             }
         }
-    }
-    private void swap(int[][] mat, int i, int j) {
-        int temp = mat[i][j];
-        mat[i][j] = mat[j][i];
-        mat[j][i] = temp;
     }
 }
