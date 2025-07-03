a221ee9a8656c1faadedfccb1263a1890f8ea1a8
HarshhT29 <132380397+HarshhT29@users.noreply.github.com>
2025-07-03 11:18:15 +0530

    Time: 7 ms (29.35%), Space: 44.9 MB (24.66%) - LeetHub

---
+++ 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.java
@@ -0,0 +1,25 @@
+class Solution {
+    private Map<Character, Character> map;
+    public char kthCharacter(int k) {
+        map = new HashMap<>();
+        for(int i=0;i<26;i++) {
+            char key = (char)(i+'a');
+            char value = i!=25?((char)('a'+i+1)):'a';
+            map.put(key, value);
+        }
+        return generate("a", k);
+    }
+    private char generate(String s, int k) {
+        if(s.length()>=k) {
+            return s.charAt(k-1);
+        }
+        StringBuilder ans = new StringBuilder(s);
+        for(char ch:s.toCharArray()) {
+            ans.append(map.get(ch));
+            if(ans.length()>=k) {
+                break;
+            }
+        }
+        return generate(ans.toString(), k);
+    }
+}
