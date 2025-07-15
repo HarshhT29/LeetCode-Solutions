d5cab60970dcef4404bcbe976620242f22dbede3
HarshhT29 <132380397+HarshhT29@users.noreply.github.com>
2025-07-15 19:59:15 +0530

    Time: 1 ms (99.12%), Space: 42.4 MB (19.87%) - LeetHub

---
+++ 3396-valid-word/3396-valid-word.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValid(String word) {
+        boolean consonant = false;
+        boolean vowel = false;
+        boolean digit = false;
+        int cnt = 0;
+
+        for(char ch : word.toCharArray()) {
+            if(!Character.isLetterOrDigit(ch)) {
+                return false;
+            }
+            if(!vowel && isVowel(ch)) {
+                vowel = true;
+            }
+            if(!consonant && Character.isLetter(ch) && !isVowel(ch)) {
+                consonant = true;
+            }
+            cnt++;
+        }
+        return cnt >= 3 && vowel && consonant;
+    }
+
+    private boolean isVowel(char ch) {
+        ch = Character.toLowerCase(ch);
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+    }
+}
