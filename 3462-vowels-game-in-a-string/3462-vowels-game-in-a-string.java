bdf90375ea8daf297d30c92ac572c4e4a102eed2
HarshhT29 <132380397+HarshhT29@users.noreply.github.com>
2025-09-12 09:45:45 +0530

    Time: 4 ms (79.79%), Space: 45.7 MB (30.18%) - LeetHub

---
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean doesAliceWin(String s) {
+        for(char ch:s.toCharArray()) {
+            switch(ch) {
+                case 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' :
+                    return true;
+                default:
+                    break;
+            }
+        }
+        return false;
+    }
+}
