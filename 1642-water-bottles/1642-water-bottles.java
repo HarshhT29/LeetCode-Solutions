ad121ab80639b9faeb39a2915ce6a902c3dea5af
HarshhT29 <132380397+HarshhT29@users.noreply.github.com>
2025-10-01 23:26:05 +0530

    Time: 0 ms (100%), Space: 40.2 MB (86.01%) - LeetHub

---
+++ 1642-water-bottles/1642-water-bottles.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int usedBottles = 0;
+        while(numBottles >= numExchange) {
+            usedBottles+=numExchange;
+            numBottles-=numExchange;
+            numBottles++;
+        }
+        return usedBottles+numBottles;
+    }
+}
